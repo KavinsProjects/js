41121981fe55b0358b0b828e5a2db4a5e0051b05
kavinsProjects <kavinkanagaraj16@gmail.com>
2026-04-06 21:55:55 +0530

    Student Id card Project

---
+++ array.js
@@ -0,0 +1,18 @@
+//array
+//array can hold different datatypes in js
+// finding  the length of array 
+//assecessing with index value 
+
+const myarry = ["lamborhini", "AMG One", "Proches","Mclaren "];
+const tripvist = ["ooty", "12", 90.0, "",true ];
+const numbers = [1, 5, 10, 15, 20];
+
+console.log(typeof myarry);
+console.log(numbers);
+
+console.log(myarry[0]); //print lam
+
+//console.log(`the lenght of give array is .${tripvis.}`);
+console.log(tripvist.length);
+const lasttipt = tripvist[tripvist.length -1];
+console.log(lasttipt);
---
+++ project.js
@@ -0,0 +1,13 @@
+const studentIdInfo = {
+    Naame: "kavin",
+    bloodGroup: 'A',
+    phoneNum: '+91755037032',
+    addressOfStud: "17/1 Rvs Nagar Cbe.",
+    classs: "|||- BSc.cs- 'A'",
+    rollNum: "231cs024"
+};
+
+console.log(studentIdInfo);
+
+console.log(`${studentIdInfo.Naame}, his Blood Group is ${studentIdInfo.bloodGroup}, phoneNum is ${studentIdInfo.phoneNum}, address is ${studentIdInfo.addressOfStud}.
+He is in ${studentIdInfo.classs} and his roll number is ${studentIdInfo.rollNum}`);
