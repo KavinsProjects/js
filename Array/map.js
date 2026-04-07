2cdd81afc13a0be2f8ebf45c240755ba7338f9b4
kavinsProjects <kavinkanagaraj16@gmail.com>
2026-04-07 18:57:44 +0530

    organizing files and folder

---
+++ Array/map.js
@@ -0,0 +1,8 @@
+// Map is object
+//new Map();  -create n map
+
+let map = new Map();
+map.set(1,"code with kavin")
+
+console.log(map);
+console.log(typeof map);
---
+++ task.js
@@ -0,0 +1,5 @@
+const str = "hello worls";
+
+let mystr = str.split('').reverse().join("");
+
+console.log(mystr);
