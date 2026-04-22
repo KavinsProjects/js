5b02ebfbd2bfea6edc11996d48e7c14f814ab21f
kavinsProjects <kavinkanagaraj16@gmail.com>
2026-04-22 15:24:57 +0530

    Bro Project completed

---
+++ JS_specific/Array_Destructuring.js
@@ -0,0 +1,15 @@
+/*
+You have an array You have an array ["Alice", "Bob", "Charlie"]. Extract the first and second elements into variables first and second. Extract the first and second elements into variables first and second.
+*/
+
+let [FirstName, SecondName, thridNumer] =  ["Alice", "Bob", "Charlie"] ;
+console.log(FirstName,SecondName);
+
+let [a,b,c]= ["red","green", "blue"];
+
+[a,b] [b,a]
+console.log(b,a);
+
+//  let arr = [1,2,3,4] = [10, 20, 30, 40]
+
+//  console.log(....4);
--- JS_specific/Asynchronous/fetch.js
+++ JS_specific/Asynchronous/fetch.js
@@ -43,7 +43,7 @@ async function fetchdata() {
       //           }
       //           const data = await response.json();
       //           console.log(data.definitions)
-      //       }
+      //       }  
       //       catch(error){
       //           console.error(error);
       //       }
